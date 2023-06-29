995d398cdc8f9962d39a360d601f146f9ec6f5e3
Fernanda Molina <misscocoway@gmail.com>
2023-06-28 19:48:55 -0600

    Added testUrl for parametrized build.

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -7,11 +7,14 @@ import static utils.ConfigDataUtility.url;
 
 public abstract class BaseTest {
     Browser browser;
+    String testUrl = System.getProperty("testUrl");
+
 
     @BeforeMethod
     public void setUp() {
         browser = AqualityServices.getBrowser();
-        browser.goTo(url);
+//        browser.goTo(url);
+        browser.goTo(testUrl);
         browser.maximize();
     }
 
